a74570d689da1e087224b7c8db5574c49ddad23e
TG-Techie <TGTechie01@gmail.com>
2019-10-20 02:34:34 -0400

    done?

--- scanner.py
+++ scanner.py
@@ -9,9 +9,9 @@ import random
 import cv2
 import bitmap
 
+num_qrs_pre_msg = 12
 
-
-def scan_for_qr():
+def scan_for_qrs():
 	"""
 	Desc: scans once for a qr code and returns a list of all the codes found;
 	"""
@@ -45,7 +45,6 @@ def scan_for_qr():
 
 	vs.stop()
 
-num_qrs = 10
 
 def exchange(username, canvas, max_msgs_power = 5, switching_time = 1):
 	"""
@@ -56,25 +55,28 @@ def exchange(username, canvas, max_msgs_power = 5, switching_time = 1):
 	arg canvas: a tkinter canvas that will be drawn on to display the bitmap;
 	kwarg max_msgs_power: log base ten of the maximum number of msages that can be passed;
 	"""
-
-	first_header = "pos=0, username='"+username#_____ will be replaced
+	num_qrs = num_qrs_pre_msg
 
 	qr_front = "[OptIn]"
+	qr_end_front = '[OptInEND]'
+
+	first_header = qr_front+"pos=0, username='"+username#_____ will be replaced
+
 	std_header = qr_front+'pos='+'_'*max_msgs_power
 
 	headers = [first_header] #messages
 	while len(headers) < num_qrs:
 		headers.append(std_header)
 
-	if len(headers) > 10**max_msgs_power:
-		return exchange(username, canvas, max_msgs_power = max_msgs_power + 1, switching_time = switching_time)
+	#if len(headers) > 10**max_msgs_power:
+	#	return exchange(username, canvas, max_msgs_power = max_msgs_power + 1, switching_time = switching_time)
 
 	my_key = bytes()
 	qrs = []
 	for index, header in enumerate(headers):
 		pos = str(index)
-		while len(pos) < num_qrs:
-			pos = '0'+pos
+		#while len(pos) < max_msgs_power:
+			#pos = '0'+pos
 
 		qr_tup = bitmap.string_and_otp_bitmap(header.replace('_'*max_msgs_power, pos), '|', 64)
 		my_key += qr_tup[0]
@@ -83,24 +85,38 @@ def exchange(username, canvas, max_msgs_power = 5, switching_time = 1):
 
 	#transmit and scan for qrs
 	last_time_qr_switched = time.monotonic()
+
 	transmition_complete_confirmed = False
 	end_qr_posted = False
 	found = []
 	should_continue = True
+
 	while should_continue:
 		if (len(found) == num_qrs) and not end_qr_posted:
-			bitmap.write_bitmap_to_canvas(bitmap.string_to_bitmap(qr_front+'[END]'), canvas)
-		elif not end_qr_posted:
+			bitmap.write_bitmap_to_canvas(bitmap.string_to_bitmap(qr_end_front)[1], canvas)
+			end_qr_posted = True
+		if transmition_complete_confirmed == False:
 			if (time.monotonic() - last_time_qr_switched) > switching_time:
 
 				bitmap.write_bitmap_to_canvas(qrs[0], canvas)
 
 				qrs.append(qrs.pop(0))
 				last_time_qr_switched = time.monotonic()
-		else:
-			pass
 
-		if
+		if len(found) < num_qrs:
+			#raw_found_qrs =
+			for qr in scan_for_qrs():
+				if qr.starswith(qr_front):
+					if qr not in found:
+						found.append(qr)
+				elif qr.startswith(qr_end_front):
+					transmition_complete_confirmed = True
+
+		if transmition_complete_confirmed and end_qr_posted:
+			break
+			
+	print(found)
+
 
 
 
